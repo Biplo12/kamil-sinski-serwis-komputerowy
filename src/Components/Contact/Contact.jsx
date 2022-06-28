3fc5eaafab49fb4c22f9eb2767b5710c37fa6e82
Biplo12 <robertbiplosek@gmail.com>
2022-06-28 02:44:39 +0200

    contact form section created with responsive design

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import customShape from "../../Assets/Images/Conact/Image/custom-shape.png";
+import location from "../../Assets/Images/Conact/Icons/location.png";
+import phone from "../../Assets/Images/Conact/Icons/phone.png";
+import mail from "../../Assets/Images/Conact/Icons/mail.png";
+import facebook from "../../Assets/Images/Conact/Icons/facebook.png";
+const Contact = () => {
+  return (
+    <div className="contact-container" id="kontakt">
+      <h1>
+        ZAPYTAJ O <span>WYCENE</span>
+      </h1>
+      <div className="contact-card">
+        <div className="contact-inner-card">
+          <div className="inner-card-label">
+            <img src={customShape} alt="custom ellipse" />
+            <div className="inner-card-content">
+              <h2>Kontakt</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas dignissimos molestiae mollitia quam error tempore.
+              </p>
+            </div>
+          </div>
+          <div className="inner-card-info">
+            <ul>
+              <li>
+                <img src={location} alt="location icon" />
+                <a href="">
+                  <span className="un">
+                    <span>Bytom,</span> Karbowska 13/1, 41-923
+                  </span>
+                </a>
+              </li>
+              <li>
+                <img src={phone} alt="location icon" />
+                <a href="callto:513292132">
+                  <span className="un">+48 513 292 132</span>
+                </a>
+              </li>
+              <li>
+                <img src={mail} alt="location icon" />
+                <a href="mailto:kamilsinskiserwis@gmail.com">
+                  <span className="un">kamilsinskiserwis@gmail.com</span>
+                </a>
+              </li>
+              <li>
+                <img src={facebook} alt="location icon" />
+                <a href="">
+                  <span className="un">/kamilsinskiserwis</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="contact-card-form-container">
+          <div className="contact-card-form">
+            <input type="text" placeholder="Imię" className="name" />
+            <input type="Email" placeholder="E-mail" className="mail" />
+            <input
+              type="phone"
+              placeholder="Numer telefonu (opcjonalne)"
+              className="phone"
+            />
+            <input type="text" placeholder="Temat" className="topic" />
+            <input
+              type="text"
+              placeholder="Treść wiadomośći"
+              className="message"
+            />
+            <button type="submit">Wyślij</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../Components/Hero/Hero";
 import WhyMe from "../Components/WhyMe/WhyMe";
 import About from "../Components/About/About";
 import Services from "../Components/Services/Services";
+import Contact from "../Components/Contact/Contact";
 const Home = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const Home = () => {
       <WhyMe />
       <About />
       <Services />
+      <Contact />
     </>
   );
 };
