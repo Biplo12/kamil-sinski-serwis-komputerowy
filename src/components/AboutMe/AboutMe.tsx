f2970e5308bee539fe449c9be902bf8054cc754e
Biplo12 <robertbiplosek@gmail.com>
2023-04-07 17:35:51 +0200

    feat: header

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 const AboutMe: React.FC = (): JSX.Element => {
   return (
-    <div className='bg-black-stalion flex h-[100vh] w-full flex-col items-center justify-center text-white'>
-      sadsa
-    </div>
+    <div className='bg-black-stalion flex h-[100vh] w-full flex-col items-center justify-center text-white'></div>
   );
 };
 export default AboutMe;
