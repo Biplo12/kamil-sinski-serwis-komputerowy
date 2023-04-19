6da893b7e40139b8c8ea01d7194332fe472571a7
Biplo12 <robertbiplosek@gmail.com>
2023-04-19 17:48:46 +0200

    feat: createorder WP

--- src/pages/api/order/createorder.ts
+++ src/pages/api/order/createorder.ts
@@ -1,46 +1,61 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { createTransport } from 'nodemailer';
+// import { createTransport } from 'nodemailer';
 
-import missingArguments from '@/utils/missingArguments';
+// import missingArguments from '@/utils/missingArguments';
+// import prisma from '@/utils/prisma';
+
+interface IOrderData {
+  data: {
+    email: string;
+    orderStatus: string;
+  };
+}
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
   try {
-    const { email } = req.body;
-    const missArgs = missingArguments({ email });
-    if (missArgs.length > 0) {
-      throw new Error(`Missing arguments: ${missArgs.join(', ')}`);
-    }
+    // const { email } = req.body;
+    // const missArgs = missingArguments({ email });
+    // if (missArgs.length > 0) {
+    //   throw new Error(`Missing arguments: ${missArgs.join(', ')}`);
+    // }
 
-    const orderId = '1234567890';
+    // const orderData: IOrderData = {
+    //   data: {
+    //     email,
+    //     orderStatus: 'NEW',
+    //   },
+    // };
 
-    const emailBody = {
-      from: email,
-      to: process.env.EMAIL_ADDRESS,
-      subject: `Potwierdzenie złoszenia zamówienia - Nie odpisuj na tę wiadomość. Numer zamówienia ${orderId}.`,
-      text: `
-        Dziękujemy za złożenie zamówienia w naszym serwisie komputerowym.
-        Numer Twojego zamówienia to: ${orderId}.
-        Sprawdź szczegóły zamówienia na stronie: https://www.example.com/order/${orderId}, 
-        lub wejdź w zakładkę Status zamówienia w naszym serwisie i wpisz numer zamówienia.
+    // const newOrder = await prisma.order.create(orderData);
+    // const { _id } = newOrder;
+    // const emailBody = {
+    //   from: process.env.EMAIL_ADDRESS,
+    //   to: email,
+    //   subject: `Potwierdzenie złoszenia zamówienia - Nie odpisuj na tę wiadomość. Numer zamówienia ${_id}.`,
+    //   text: `
+    //     Dziękujemy za złożenie zamówienia w naszym serwisie komputerowym.
+    //     Numer Twojego zamówienia to: ${_id}.
+    //     Sprawdź szczegóły zamówienia na stronie: https://www.example.com/order/${_id},
+    //     lub wejdź w zakładkę Status zamówienia w naszym serwisie i wpisz numer zamówienia.
 
-        Pozdrawiamy,
-        Zespół serwisu komputerowego kamilsinski.pl
-      `,
-    };
+    //     Pozdrawiamy,
+    //     Zespół serwisu komputerowego kamilsinski.pl
+    //   `,
+    // };
 
-    const transporter = createTransport({
-      port: 465,
-      host: 'smtp.gmail.com',
-      auth: {
-        user: process.env.SENDER_EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+    // const transporter = createTransport({
+    //   port: 465,
+    //   host: 'smtp.gmail.com',
+    //   auth: {
+    //     user: process.env.SENDER_EMAIL_ADDRESS,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // });
 
-    await transporter.sendMail(emailBody);
+    // await transporter.sendMail(emailBody);
 
     res.status(200).json({ statusCode: 200, message: 'Success' });
   } catch (err) {
